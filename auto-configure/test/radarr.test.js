5b0ebc6008d4c3ada8548611f4cff4bec3cf01b6
shouvikbasu <babanomania@gmail.com>
2022-07-24 15:33:31 +0530

    added extra checks in auto-configure

--- auto-configure/test/radarr.test.js
+++ auto-configure/test/radarr.test.js
@@ -5,6 +5,8 @@ const api = request(process.env.RADARR_URL);
 
 describe("Radarr", function () {
   let apiKey;
+  let rootFolders;
+  let downloadclients;
 
   before("Get Api-Key", async function () {
     await api
@@ -23,81 +25,103 @@ describe("Radarr", function () {
       });
   });
 
-  it("Set Root Folder", function () {
+  before("Check Root Folders", function () {
     return api
-      .post("/api/v3/rootFolder")
+      .get("/api/v3/rootFolder")
       .set("X-Api-Key", apiKey)
       .set("Accept", "application/json")
-      .send({
-        path: "/data/",
-      })
-      .expect(201);
+      .expect(200)
+      .then((res) => (rootFolders = JSON.parse(res.text)));
   });
 
-  it("Set qflood Client", function () {
+  it("Set Root Folder", function () {
+    return rootFolders && rootFolders.length === 0
+      ? api
+          .post("/api/v3/rootFolder")
+          .set("X-Api-Key", apiKey)
+          .set("Accept", "application/json")
+          .send({
+            path: "/data/",
+          })
+          .expect(201)
+      : this.skip();
+  });
+
+  before("Check Download Clients", function () {
     return api
-      .post("/api/v3/downloadclient?")
+      .get("/api/v3/downloadclient?")
       .set("X-Api-Key", apiKey)
       .set("Accept", "application/json")
-      .send({
-        enable: true,
-        protocol: "torrent",
-        priority: 1,
-        removeCompletedDownloads: true,
-        removeFailedDownloads: true,
-        name: "qflood",
-        fields: [
-          {
-            name: "host",
-            value: "qflood",
-          },
-          {
-            name: "port",
-            value: 3000,
-          },
-          {
-            name: "useSsl",
-            value: false,
-          },
-          {
-            name: "urlBase",
-          },
-          {
-            name: "username",
-          },
-          {
-            name: "password",
-          },
-          {
-            name: "destination",
-          },
-          {
-            name: "tags",
-            value: ["radarr"],
-          },
-          {
-            name: "postImportTags",
-          },
-          {
-            name: "additionalTags",
-            value: [],
-          },
-          {
-            name: "addPaused",
-            value: false,
-          },
-        ],
-        implementationName: "Flood",
-        implementation: "Flood",
-        configContract: "FloodSettings",
-        infoLink: "https://wiki.servarr.com/radarr/supported#flood",
-        message: {
-          message:
-            "Radarr will handle automatic removal of torrents based on the current seed criteria in Settings -> Indexers",
-          type: "info",
-        },
-        tags: [],
-      })
-      .expect(201);
+      .expect(200)
+      .then((res) => (downloadclients = JSON.parse(res.text)));
+  });
+
+  it("Set Download Client", function () {
+    return downloadclients && downloadclients.length === 0
+      ? api
+          .post("/api/v3/downloadclient?")
+          .set("X-Api-Key", apiKey)
+          .set("Accept", "application/json")
+          .send({
+            enable: true,
+            protocol: "torrent",
+            priority: 1,
+            removeCompletedDownloads: true,
+            removeFailedDownloads: true,
+            name: "qflood",
+            fields: [
+              {
+                name: "host",
+                value: "qflood",
+              },
+              {
+                name: "port",
+                value: 3000,
+              },
+              {
+                name: "useSsl",
+                value: false,
+              },
+              {
+                name: "urlBase",
+              },
+              {
+                name: "username",
+              },
+              {
+                name: "password",
+              },
+              {
+                name: "destination",
+              },
+              {
+                name: "tags",
+                value: ["radarr"],
+              },
+              {
+                name: "postImportTags",
+              },
+              {
+                name: "additionalTags",
+                value: [],
+              },
+              {
+                name: "addPaused",
+                value: false,
+              },
+            ],
+            implementationName: "Flood",
+            implementation: "Flood",
+            configContract: "FloodSettings",
+            infoLink: "https://wiki.servarr.com/radarr/supported#flood",
+            message: {
+              message:
+                "Radarr will handle automatic removal of torrents based on the current seed criteria in Settings -> Indexers",
+              type: "info",
+            },
+            tags: [],
+          })
+          .expect(201)
+      : this.skip();
   });
 });
--- auto-configure/test/sonarr.test.js
+++ auto-configure/test/sonarr.test.js
@@ -5,6 +5,8 @@ const api = request(process.env.SONARR_URL);
 
 describe("Sonarr", function () {
   let apiKey;
+  let rootFolders;
+  let downloadclients;
 
   before("Get Api-Key", async function () {
     await api
@@ -23,81 +25,103 @@ describe("Sonarr", function () {
       });
   });
 
-  it("Set Root Folder", function () {
+  before("Check Root Foldesr", function () {
     return api
-      .post("/api/v3/rootFolder")
+      .get("/api/v3/rootFolder")
       .set("X-Api-Key", apiKey)
       .set("Accept", "application/json")
-      .send({
-        path: "/data/",
-      })
-      .expect(201);
+      .expect(200)
+      .then((res) => (rootFolders = JSON.parse(res.text)));
   });
 
-  it("Set qflood Client", function () {
+  it("Set Root Folder", function () {
+    return rootFolders && rootFolders.length === 0
+      ? api
+          .post("/api/v3/rootFolder")
+          .set("X-Api-Key", apiKey)
+          .set("Accept", "application/json")
+          .send({
+            path: "/data/",
+          })
+          .expect(201)
+      : this.skip();
+  });
+
+  before("Check Download Clients", function () {
     return api
-      .post("/api/v3/downloadclient?")
+      .get("/api/v3/downloadclient?")
       .set("X-Api-Key", apiKey)
       .set("Accept", "application/json")
-      .send({
-        enable: true,
-        protocol: "torrent",
-        priority: 1,
-        removeCompletedDownloads: true,
-        removeFailedDownloads: true,
-        name: "qflood",
-        fields: [
-          {
-            name: "host",
-            value: "qflood",
-          },
-          {
-            name: "port",
-            value: 3000,
-          },
-          {
-            name: "useSsl",
-            value: false,
-          },
-          {
-            name: "urlBase",
-          },
-          {
-            name: "username",
-          },
-          {
-            name: "password",
-          },
-          {
-            name: "destination",
-          },
-          {
-            name: "tags",
-            value: ["sonarr"],
-          },
-          {
-            name: "postImportTags",
-          },
-          {
-            name: "additionalTags",
-            value: [],
-          },
-          {
-            name: "startOnAdd",
-            value: true,
-          },
-        ],
-        implementationName: "Flood",
-        implementation: "Flood",
-        configContract: "FloodSettings",
-        infoLink: "https://wiki.servarr.com/sonarr/supported#flood",
-        message: {
-          message:
-            "Sonarr will handle automatic removal of torrents based on the current seed criteria in Settings -> Indexers",
-          type: "info",
-        },
-        tags: [],
-      })
-      .expect(201);
+      .expect(200)
+      .then((res) => (downloadclients = JSON.parse(res.text)));
+  });
+
+  it("Set Download Client", function () {
+    return downloadclients && downloadclients.length === 0
+      ? api
+          .post("/api/v3/downloadclient?")
+          .set("X-Api-Key", apiKey)
+          .set("Accept", "application/json")
+          .send({
+            enable: true,
+            protocol: "torrent",
+            priority: 1,
+            removeCompletedDownloads: true,
+            removeFailedDownloads: true,
+            name: "qflood",
+            fields: [
+              {
+                name: "host",
+                value: "qflood",
+              },
+              {
+                name: "port",
+                value: 3000,
+              },
+              {
+                name: "useSsl",
+                value: false,
+              },
+              {
+                name: "urlBase",
+              },
+              {
+                name: "username",
+              },
+              {
+                name: "password",
+              },
+              {
+                name: "destination",
+              },
+              {
+                name: "tags",
+                value: ["sonarr"],
+              },
+              {
+                name: "postImportTags",
+              },
+              {
+                name: "additionalTags",
+                value: [],
+              },
+              {
+                name: "startOnAdd",
+                value: true,
+              },
+            ],
+            implementationName: "Flood",
+            implementation: "Flood",
+            configContract: "FloodSettings",
+            infoLink: "https://wiki.servarr.com/sonarr/supported#flood",
+            message: {
+              message:
+                "Sonarr will handle automatic removal of torrents based on the current seed criteria in Settings -> Indexers",
+              type: "info",
+            },
+            tags: [],
+          })
+          .expect(201)
+      : this.skip();
   });
 });
